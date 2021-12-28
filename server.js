1be418871663b2872f9b49c2561667f14c72b687
Patryk-Szymula <pat.szymula@gmail.com>
2021-12-28 20:38:03 +0100

    commit zmiany pliku do zadania

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ app.get("/", function (req, res) {
     res.send("<h1>my app on heroku - kolejna mała zmiana w pliku!</h1>")
 })
 
+var data = {imie: "Patryk", nazwisko: "Szymula", klasa: "3I2", grupa: "1"}
+
+app.get("/data", function (req, res) {
+    res.send(data)
+})
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
